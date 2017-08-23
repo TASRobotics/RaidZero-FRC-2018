c79550c79ef65c5dea2d1d47d1521715a855c043
qsctr <brettonchen@gmail.com>
2017-08-23 13:00:14 +0800

    Initialize project

---
+++ src/org/usfirst/frc/team4253/robot2017/Robot.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team4253.robot2017;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot {
+
+	public Robot() {
+	    
+	}
+
+	@Override
+	public void robotInit() {
+	    
+	}
+
+	@Override
+	public void autonomous() {
+	    
+	}
+
+	@Override
+	public void operatorControl() {
+		while (isOperatorControl() && isEnabled()) {
+			
+		}
+	}
+
+	@Override
+	public void test() {
+	    
+	}
+	
+}
