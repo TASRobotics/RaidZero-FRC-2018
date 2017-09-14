d72bda7d0e89f98ee7dcd0f4a64a2c28d64d3774
qsctr <brettonchen@gmail.com>
2017-09-14 10:53:15 +0800

    Add Climb and GearDoor class

---
+++ src/org/usfirst/frc/team4253/robot2017/Climb.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4253.robot2017;
+
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
+
+public class Climb {
+
+    private static final double POWER_MULTIPLIER = 1;
+    
+    private CANTalon motor;
+    
+    public Climb(int motor1ID, int motor2ID) {
+        motor = new CANTalon(motor1ID);
+        motor.changeControlMode(TalonControlMode.PercentVbus);
+        motor.enableBrakeMode(true);
+        motor.setInverted(true);
+        
+        CANTalon motor2 = new CANTalon(motor2ID);
+        motor2.changeControlMode(TalonControlMode.PercentVbus);
+        motor2.set(motor1ID);
+        motor2.enableBrakeMode(true);
+        motor2.reverseOutput(true);
+    }
+    
+    public void up() {
+        motor.set(POWER_MULTIPLIER);
+    }
+    
+    public void upHalfPower() {
+        motor.set(POWER_MULTIPLIER / 2);
+    }
+    
+    public void stop() {
+        motor.set(0);
+    }
+    
+    public void down() {
+        motor.set(-POWER_MULTIPLIER);
+    }
+    
+}
---
+++ src/org/usfirst/frc/team4253/robot2017/GearDoor.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team4253.robot2017;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class GearDoor {
+    
+    private DoubleSolenoid piston;
+    
+    public GearDoor(int forwardChannel, int reverseChannel) {
+        piston = new DoubleSolenoid(forwardChannel, reverseChannel);
+    }
+    
+    public void open() {
+        piston.set(DoubleSolenoid.Value.kForward);
+    }
+    
+    public void close() {
+        piston.set(DoubleSolenoid.Value.kReverse);
+    }
+    
+}
