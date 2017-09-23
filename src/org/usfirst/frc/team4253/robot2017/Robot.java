de78337fe837a7f9cb7a78b3845f6ca547ada3c7
qsctr <brettonchen@gmail.com>
2017-09-24 00:19:47 +0800

    Add javadoc for Robot and Settings classes

--- src/org/usfirst/frc/team4253/robot2017/Robot.java
+++ src/org/usfirst/frc/team4253/robot2017/Robot.java
@@ -7,8 +7,23 @@ import org.usfirst.frc.team4253.robot2017.teleop.Teleop;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 
+/**
+ * The main class of the program.
+ * 
+ * <p>The methods in this class are automatically called by the WPI library when the robot switches
+ * to a different mode.
+ */
 public class Robot extends SampleRobot {
 
+    /**
+     * Initializes everything.
+     * 
+     * <p>This method is automatically called when the roboRIO starts up. It will only be called
+     * once.
+     * 
+     * <p>This is similar to a constructor, but it is better to put any code in here rather than the
+     * constructor because WPILib does some initialization stuff before calling this method.
+     */
     @Override
     public void robotInit() {
         Components.initialize();
@@ -16,12 +31,23 @@ public class Robot extends SampleRobot {
         Teleop.initialize();
     }
 
+    /**
+     * Runs autonomous code.
+     * 
+     * <p>This method is automatically called once when autonomous mode is started.
+     */
     @Override
     public void autonomous() {
         Auto.setup();
         Auto.run();
     }
 
+    /**
+     * Runs teleop (remote control) code.
+     * 
+     * <p>This method is automatically called once when teleop mode is started. However, it keeps
+     * running until teleop mode is over, because it has a loop inside.
+     */
     @Override
     public void operatorControl() {
         Teleop.setup();
--- src/org/usfirst/frc/team4253/robot2017/Settings.java
+++ src/org/usfirst/frc/team4253/robot2017/Settings.java
@@ -1,9 +1,25 @@
 package org.usfirst.frc.team4253.robot2017;
 
+/**
+ * Some robot-wide settings.
+ * 
+ * <p>Other classes can use the constants defined in this class to have different behavior based on
+ * different settings of the robot. This way the settings can be easily changed in one location.
+ */
 public class Settings {
 
+    /**
+     * The version of the robot.
+     * 
+     * <p>There are some minor differences between the several iterations of the robot that were
+     * built. Code that is different depending on the version should check the value of this
+     * constant to determine the robot version.
+     */
     public static final Version VERSION = Version.V4;
 
+    /**
+     * The possible versions of the robot.
+     */
     public static enum Version { V2, V3, V4 }
 
 }
