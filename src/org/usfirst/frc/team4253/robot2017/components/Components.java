28967be94ecba1d2bf47b2f1908df151b909343e
qsctr <brettonchen@gmail.com>
2017-09-25 00:31:41 +0800

    Add some javadoc for Components class

--- src/org/usfirst/frc/team4253/robot2017/components/Components.java
+++ src/org/usfirst/frc/team4253/robot2017/components/Components.java
@@ -2,6 +2,14 @@ package org.usfirst.frc.team4253.robot2017.components;
 
 import edu.wpi.first.wpilibj.Compressor;
 
+/**
+ * The components of the robot.
+ * 
+ * <p>All the variables and methods in this class should be static, because there is only one set of
+ * components throughout the whole robot. Do not construct an instance of this class.
+ * 
+ * <p>Make sure you call the {@code initialize()} method before accessing any components.
+ */
 public class Components {
 
     private static Drive drive;
@@ -10,6 +18,12 @@ public class Components {
     private static Intake intake;
     private static Compressor compressor;
 
+    /**
+     * Initializes each component by calling its constructor.
+     * 
+     * <p>Make sure you call this method before accessing any components, as they will be null
+     * before this method is called.
+     */
     public static void initialize() {
         drive = new Drive(1, 2, 3, 4, 5, 6, 0, 1);
         climb = new Climb(15, 13);
