73e2017f78a937b4b1c0661e8fc10ac2b66dce2a
Bretton Chen <19111183@tasfiledomain.tas.edu.tw>
2017-10-06 13:47:56 +0800

    Add javadoc for Teleop class

--- src/org/usfirst/frc/team4253/robot2017/teleop/Teleop.java
+++ src/org/usfirst/frc/team4253/robot2017/teleop/Teleop.java
@@ -4,23 +4,56 @@ import org.usfirst.frc.team4253.robot2017.components.Components;
 import org.usfirst.frc.team4253.robot2017.teleop.Controller.Axis;
 import org.usfirst.frc.team4253.robot2017.teleop.Controller.Button;
 
+/**
+ * Teleop (remote control) specific code for the robot.
+ */
 public class Teleop {
 
+    /**
+     * The controller on port 0.
+     */
     private static Controller firstController;
+    /**
+     * The controller on port 1.
+     */
     private static Controller secondController;
     private static TeleopDrive teleopDrive;
 
+    /**
+     * Initializes the teleop-specific components.
+     * 
+     * <p>Make sure this method has been called before calling the other methods.
+     * 
+     * <p>The difference between this method and {@link #setup()} is that this should only be run
+     * once at the beginning, but {@link #setup()} should be called once every time the robot is
+     * switched to autonomous mode.
+     */
     public static void initialize() {
         firstController = new Controller(0);
         secondController = new Controller(1);
         teleopDrive = new TeleopDrive(Components.getDrive());
     }
 
+    /**
+     * Configures the components for use in teleop mode.
+     * 
+     * <p>This should be called once every time the robot is switched to autonomous mode, before
+     * calling {@link #run()}.
+     * 
+     * <p>The difference between this method and {@link #initialize()} is that this should be called
+     * once every time the robot is switched to autonomous mode, but {@link #initialize()} should
+     * only be run once at the beginning.
+     */
     public static void setup() {
         teleopDrive.setup();
         Components.getCompressor().start();
     }
 
+    /**
+     * Runs the teleop code.
+     * 
+     * <p>This method should be called in a loop, after calling {@link #setup()} once.
+     */
     public static void run() {
         // drive controls
         if (firstController.isPressed(Button.RB)) {
