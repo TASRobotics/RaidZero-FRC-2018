c3486d01f30b83bda1ed4352dbc467a227003657
qsctr <brettonchen@gmail.com>
2017-10-16 13:48:17 +0800

    Add javadoc for Auto class

--- src/org/usfirst/frc/team4253/robot2017/auto/Auto.java
+++ src/org/usfirst/frc/team4253/robot2017/auto/Auto.java
@@ -3,20 +3,45 @@ package org.usfirst.frc.team4253.robot2017.auto;
 import org.usfirst.frc.team4253.robot2017.auto.AutoChooser.Color;
 import org.usfirst.frc.team4253.robot2017.components.Components;
 
+/**
+ * Autonomous specific code for the robot.
+ */
 public class Auto {
 
     private static AutoDrive autoDrive;
 
+    /**
+     * Initializes the autonomous-specific components.
+     * 
+     * <p>Make sure this method has been called before calling the other methods.
+     * 
+     * <p>The difference between this method and {@link #setup()} is that this should only be run
+     * once at the beginning, but {@link #setup()} should be called once every time the robot is
+     * switched to autonomous mode.
+     */
     public static void initialize() {
         AutoChooser.initialize();
         autoDrive = new AutoDrive(Components.getDrive());
     }
 
+    /**
+     * Configures the components for use in autonomous mode.
+     * 
+     * <p>This should be called once every time the robot is switched to autonomous mode, before
+     * calling {@link #run()}.
+     * 
+     * <p>The difference between this method and {@link #initialize()} is that this should be called
+     * once every time the robot is switched to autonomous mode, but {@link #initialize()} should
+     * only be run once at the beginning.
+     */
     public static void setup() {
         autoDrive.setup();
         Components.getCompressor().start();
     }
 
+    /**
+     * Runs the autonomous code once.
+     */
     public static void run() {
         Color color = AutoChooser.getColor();
         switch (AutoChooser.getMode()) {
@@ -37,10 +62,18 @@ public class Auto {
         }
     }
 
+    /**
+     * Crosses the line.
+     */
     private static void crossLine() {
         autoDrive.moveMeters(4, 5);
     }
 
+    /**
+     * Places the gear on the center peg of the airship, then crosses the line.
+     * 
+     * @param color the alliance the robot is on
+     */
     private static void deliverCenterGear(Color color) {
         boolean red = color == Color.Red;
         autoDrive.moveInches(44, 1);
@@ -60,6 +93,12 @@ public class Auto {
         autoDrive.moveMeters(8, 3);
     }
 
+    /**
+     * Places the gear on a side peg of the airship, then crosses the line.
+     * 
+     * @param color the alliance the robot is on
+     * @param side which peg to put the gear on (left or right)
+     */
     private static void deliverSideGear(Color color, Side side) {
         boolean leftSide = side == Side.Left;
         // red left or blue right = gear loading side
@@ -91,6 +130,9 @@ public class Auto {
         return false;
     }
 
+    /**
+     * The side of a peg on the airship (left or right).
+     */
     private static enum Side { Left, Right }
 
 }
