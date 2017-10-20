bbc5c76d75ce81d3e25a71e4d007d072284ea078
qsctr <brettonchen@gmail.com>
2017-10-20 13:01:24 +0800

    Add javadoc for AutoChooser

--- src/org/usfirst/frc/team4253/robot2017/auto/AutoChooser.java
+++ src/org/usfirst/frc/team4253/robot2017/auto/AutoChooser.java
@@ -4,11 +4,28 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * SmartDashboard controls for selecting the correct autonomous routine to run.
+ */
 public class AutoChooser {
 
+    /**
+     * SendableChooser for selecting the alliance the robot is on.
+     */
     private static SendableChooser<Color> colorChooser;
+    /**
+     * SendableChooser for selecting the autonomous mode to run.
+     */
     private static SendableChooser<AutoMode> modeChooser;
 
+    /**
+     * Attempts to auto-detect the alliance the robot is on, and then places the selection controls
+     * onto the SmartDashboard.
+     * 
+     * <p>If the auto-detection is successful, then the detected alliance will be pre-selected on
+     * the autonomous chooser. Otherwise, the default alliance will be red. You can always override
+     * the detected alliance. The default autonomous mode is to do nothing.
+     */
     public static void initialize() {
         colorChooser = new SendableChooser<>();
         switch (DriverStation.getInstance().getAlliance()) {
@@ -38,16 +55,32 @@ public class AutoChooser {
         SmartDashboard.putData("Mode", modeChooser);
     }
 
+    /**
+     * Returns the selected alliance color on the SmartDashboard.
+     * 
+     * @return the selected alliance
+     */
     public static Color getColor() {
         return colorChooser.getSelected();
     }
 
+    /**
+     * Returns the selected autonomous mode on the SmartDashboard.
+     * 
+     * @return the selected autonomous mode
+     */
     public static AutoMode getMode() {
         return modeChooser.getSelected();
     }
 
+    /**
+     * An alliance color.
+     */
     public static enum Color { Red, Blue }
 
+    /**
+     * An autonomous routine.
+     */
     public static enum AutoMode {
         DoNothing,
         CrossLine,
