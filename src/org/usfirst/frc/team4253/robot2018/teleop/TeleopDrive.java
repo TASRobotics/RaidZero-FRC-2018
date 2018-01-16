735bd221674f4b1e2ff007611922994a633164cb
Calvin-S <calvins19110200@tas.tw>
2018-01-16 16:52:37 +0800

    Added Comments for TeleopDrive

--- src/org/usfirst/frc/team4253/robot2018/teleop/TeleopDrive.java
+++ src/org/usfirst/frc/team4253/robot2018/teleop/TeleopDrive.java
@@ -9,15 +9,29 @@ public class TeleopDrive {
     private Drive drive;
     private DifferentialDrive differentialDrive;
 
+    /**
+     * Constructs a differentialDrive object and assigns up the motors from the drive param.
+     * 
+     * @param drive the Drive object to use
+     */
     public TeleopDrive(Drive drive) {
         this.drive = drive;
         differentialDrive = new DifferentialDrive(drive.getLeftMotor(), drive.getRightMotor());
     }
 
+    /**
+     * <p> Sets up the TeleopDrive settings. This should be called when teleopDrive starts.
+     */
     public void setup() {
         drive.setLowGear();
     }
 
+    /**
+     * drives the robot using tankDrive
+     * 
+     * @param leftInput the robot's left side speed
+     * @param rightInput the robot's right side speed
+     */
     public void drive(double leftInput, double rightInput) {
         differentialDrive.tankDrive(-leftInput, -rightInput);
     }
