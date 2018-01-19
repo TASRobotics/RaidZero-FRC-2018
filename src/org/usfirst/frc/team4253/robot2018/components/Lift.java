ac7f1a99d1e95f3afe423d142590b2e4b40a8860
Calvin Shyu <19110200@tasfiledomain.tas.edu.tw>
2018-01-19 18:30:24 +0800

    Change constant name to convention

--- src/org/usfirst/frc/team4253/robot2018/components/Lift.java
+++ src/org/usfirst/frc/team4253/robot2018/components/Lift.java
@@ -3,26 +3,22 @@ package org.usfirst.frc.team4253.robot2018.components;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import java.util.concurrent.TimeUnit;
 
 public class Lift {
-    
+
     private TalonSRX leaderMotor;
     private TalonSRX follower;
-    
-    private final int targetVel = 1515; // can be changed
-    private final int targetAccel = 6060;
-    
-    private final int timeout = 10;
-    
-    private final double PValue = 0.0;
-    private final double IValue = 0.0;
-    private final double DValue = 0.0;
-    private final double FValue = 0.0;
-    
+
+    private final static int TARGET_VEL = 1515; // can be changed
+    private final static int TARGET_ACCEL = 6060;
+
+    private final double P_VALUE = 0.0;
+    private final double I_VALUE = 0.0;
+    private final double D_VALUE = 0.0;
+    private final double F_VALUE = 0.0;
+
     // # of motors can be changed by adding more slave motors
-    
+
     /**
      * Constructs a Lift object and sets up the lift motors.
      * 
@@ -32,34 +28,31 @@ public class Lift {
     public Lift(int leaderID, int followerID) {
         leaderMotor = new TalonSRX(leaderID);
         follower = new TalonSRX(followerID);
-        
+
         follower.set(ControlMode.Follower, leaderID);
-        
+
         leaderMotor.setNeutralMode(NeutralMode.Brake);
         follower.setNeutralMode(NeutralMode.Brake);
-        
-        leaderMotor.configMotionCruiseVelocity( targetVel, timeout );
-        leaderMotor.configMotionAcceleration( targetAccel, timeout );
-        
-        leaderMotor.setSelectedSensorPosition(0, 0, timeout);
-        follower.setSelectedSensorPosition(0, 0, timeout);
-        
-        leaderMotor.config_kP(0, PValue, timeout);
-        leaderMotor.config_kI(0, IValue, timeout);
-        leaderMotor.config_kD(0, DValue, timeout);
-        leaderMotor.config_kF(0, FValue, timeout);
+
+        leaderMotor.configMotionCruiseVelocity(TARGET_VEL, MotorSettings.TIMEOUT);
+        leaderMotor.configMotionAcceleration(TARGET_ACCEL, MotorSettings.TIMEOUT);
+
+        leaderMotor.setSelectedSensorPosition(0, 0, MotorSettings.TIMEOUT);
+        follower.setSelectedSensorPosition(0, 0, MotorSettings.TIMEOUT);
+
+        leaderMotor.config_kP(0, P_VALUE, MotorSettings.TIMEOUT);
+        leaderMotor.config_kI(0, I_VALUE, MotorSettings.TIMEOUT);
+        leaderMotor.config_kD(0, D_VALUE, MotorSettings.TIMEOUT);
+        leaderMotor.config_kF(0, F_VALUE, MotorSettings.TIMEOUT);
     }
-    
+
     /**
-     * moves the lift to the target position through motion magic.
+     * Moves the lift to the target position through motion magic.
      * 
-     * @param targetPos the target position for the lift.
+     * @param targetPos the target position for the lift
      */
     public void move(double targetPos) {
-        leaderMotor.set(ControlMode.MotionMagic, targetPos);  
-        
-        try { TimeUnit.MILLISECONDS.sleep(10);   // may not be needed
-        } catch(Exception e) {}
+        leaderMotor.set(ControlMode.MotionMagic, targetPos);
     }
-    
+
 }
