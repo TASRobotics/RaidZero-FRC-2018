a1f7202231395b3c33884b1fa03e5fb8ca2e314c
Kof223 <fidoko204@gmail.com>
2018-01-20 18:34:24 +0800

    Add changes reflecting what design team have in mind. Add comments to methods.

--- src/org/usfirst/frc/team4253/robot2018/components/Intake.java
+++ src/org/usfirst/frc/team4253/robot2018/components/Intake.java
@@ -3,47 +3,56 @@ package org.usfirst.frc.team4253.robot2018.components;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Intake {
-    private static double HOLD_VAL = 0.2;
+
     private TalonSRX leaderMotor;
     private TalonSRX follower;
-    private boolean isHolding;
-    
-    public Intake(int leaderID, int followerID) {
+    private DoubleSolenoid claw;
+    private static final double IDLE_POWER = 0.1;
+
+    /**
+     * 
+     * @param leaderID the ID for the leader motor
+     * @param followerID the ID for the follower motor
+     * @param forwardChannel the pneumatic channel for forward
+     * @param backwardChannel the pneumatic channel for backward
+     */
+    public Intake(int leaderID, int followerID, int forwardChannel, int backwardChannel) {
         leaderMotor = new TalonSRX(leaderID);
         follower = new TalonSRX(followerID);
-        
+        claw = new DoubleSolenoid(forwardChannel, backwardChannel);
+
         leaderMotor.setNeutralMode(NeutralMode.Brake);
         follower.setNeutralMode(NeutralMode.Brake);
-        
+
         follower.set(ControlMode.Follower, leaderID);
-        
-        isHolding = false;
+
+        claw.set(DoubleSolenoid.Value.kReverse);
     }
-    
-    public void grab() {
+
+    /**
+     * Closes the claw
+     */
+    public void closeClaw() {
         leaderMotor.set(ControlMode.PercentOutput, 1.0);
+        claw.set(DoubleSolenoid.Value.kReverse);
     }
-    
-    public void releaseMotors() {
+
+    /**
+     * Opens the claw
+     */
+    public void openClaw() {
         leaderMotor.set(ControlMode.PercentOutput, -1.0);
+        claw.set(DoubleSolenoid.Value.kForward);
     }
-    
-    public void holdMotors() {
-        if (isHolding) {
-            leaderMotor.set(ControlMode.PercentOutput, HOLD_VAL);
-        }
-        else {
-            leaderMotor.set(ControlMode.PercentOutput, 0);
-        }
-    }
-    
-    public void hold() {
-        isHolding = true;
-    }
-    
-    public void release() {
-        isHolding = false;
+
+    /**
+     * Sets the motor to a lower output when idle
+     */
+    public void idle() {
+        leaderMotor.set(ControlMode.PercentOutput, IDLE_POWER);
+        claw.set(DoubleSolenoid.Value.kReverse);
     }
 }
