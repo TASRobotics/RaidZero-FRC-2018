d80c56a6f6ecbf45051455c054812e26ac16bca3
Kof223 <fidoko204@gmail.com>
2018-01-20 16:38:16 +0800

    Add geogebra conversion stuff. Also added autoAngle

--- src/org/usfirst/frc/team4253/robot2018/auto/AutoDrive.java
+++ src/org/usfirst/frc/team4253/robot2018/auto/AutoDrive.java
@@ -8,22 +8,25 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 public class AutoDrive {
-    
+
     private static final double AUTO_STRAIGHT_P = 0.08;
     private static final double AUTO_STRIAGHT_D = 0.0004;
+    private static final double AUTO_ANGLE_P = 0.08;
+    private static final double AUTO_ANGLE_D = 0.0004;
     private static final int MAX_MODIFIER = 200;
-    
+
     private static final int DEFAULT_VEL = 1072;
     private static final int DEFAULT_ACCEL = 2144;
-    
+
     private double currentAngle;
     private double currentAngularRate;
     private double autoStraightModifier;
-    
+    private double autoAngleModifier;
+
     private TalonSRX rightMotor;
     private TalonSRX leftMotor;
     private PigeonIMU pigeon;
-    
+
     /**
      * Constructs an AutoDrive object.
      * 
@@ -34,7 +37,7 @@ public class AutoDrive {
         leftMotor = drive.getLeftMotor();
         pigeon = drive.getPigeon();
     }
-    
+
     /**
      * Sets up any needed settings
      * 
@@ -45,7 +48,7 @@ public class AutoDrive {
         rightMotor.setSelectedSensorPosition(0, MotorSettings.PID_IDX, MotorSettings.TIMEOUT);
         leftMotor.setSelectedSensorPosition(0, MotorSettings.PID_IDX, MotorSettings.TIMEOUT);
     }
-    
+
     /**
      * Moves the robot to the target position
      * 
@@ -53,52 +56,86 @@ public class AutoDrive {
      */
     public void moveStraight(int targetPos) {
         autoStraight();
-        rightMotor.set(ControlMode.MotionMagic, targetPos); 
+        rightMotor.configMotionCruiseVelocity(DEFAULT_VEL + (int) autoStraightModifier,
+            MotorSettings.TIMEOUT);
+        rightMotor.configMotionAcceleration(DEFAULT_ACCEL + (int) autoStraightModifier,
+            MotorSettings.TIMEOUT);
+        leftMotor.configMotionCruiseVelocity(DEFAULT_VEL + (int) autoStraightModifier,
+            MotorSettings.TIMEOUT);
+        leftMotor.configMotionAcceleration(DEFAULT_ACCEL + (int) autoStraightModifier,
+            MotorSettings.TIMEOUT);
+
+        rightMotor.set(ControlMode.MotionMagic, targetPos);
         leftMotor.set(ControlMode.MotionMagic, targetPos);
     }
-    
+
     /**
      * Moves the robot in a curve
      * 
      * @param targets the array containing target velocity and acceleration.
      * @param targetPos the target encoder position to move the robot to.
      */
-    public void moveCurve(int[] targets, int targetPos) {
-        rightMotor.configMotionCruiseVelocity(targets[0], MotorSettings.TIMEOUT);
-        leftMotor.configMotionCruiseVelocity(targets[1], MotorSettings.TIMEOUT);
-        rightMotor.configMotionAcceleration(targets[2], MotorSettings.TIMEOUT);
-        leftMotor.configMotionAcceleration(targets[3], MotorSettings.TIMEOUT);
+    public void moveCurve(GeoGebraEntry[] targets, int targetPos) {
+        GeoGebraEntry current = targets[determineInterval(targetPos, targets.length)];
+        int[] currentTargets = convertToMotorValues(current.getPercentDifference(), targetPos);
+        autoAngle(current.getAngle());
+
+        leftMotor.configMotionCruiseVelocity(currentTargets[0] + (int) autoAngleModifier,
+            MotorSettings.TIMEOUT);
+        rightMotor.configMotionCruiseVelocity(currentTargets[1] + (int) autoAngleModifier,
+            MotorSettings.TIMEOUT);
+        leftMotor.configMotionAcceleration(currentTargets[2] + (int) autoAngleModifier,
+            MotorSettings.TIMEOUT);
+        rightMotor.configMotionAcceleration(currentTargets[3] + (int) autoAngleModifier,
+            MotorSettings.TIMEOUT);
 
         rightMotor.set(ControlMode.MotionMagic, targetPos);
         leftMotor.set(ControlMode.MotionMagic, targetPos);
     }
-    
+
     /**
      * Straightens the robot based of the gyro.
+     * <p>This changes the {@link #autoStraightModifier} so that the robot moves straight.
      */
     public void autoStraight() {
         PigeonIMU.FusionStatus fusionStatus = new PigeonIMU.FusionStatus();
-        double [] xyz_dps = new double [3];
+        double[] xyz_dps = new double[3];
         pigeon.getRawGyro(xyz_dps);
         pigeon.getFusedHeading(fusionStatus);
-        
+
         currentAngle = fusionStatus.heading;
         currentAngularRate = xyz_dps[2];
-        
-        autoStraightModifier = (0 - currentAngle) * AUTO_STRAIGHT_P - (currentAngularRate) * AUTO_STRIAGHT_D;
+
+        autoStraightModifier =
+            (0 - currentAngle) * AUTO_STRAIGHT_P - (currentAngularRate) * AUTO_STRIAGHT_D;
         autoStraightModifier = limit(autoStraightModifier);
-        
-        rightMotor.configMotionCruiseVelocity(DEFAULT_VEL + (int) autoStraightModifier, MotorSettings.TIMEOUT);
-        rightMotor.configMotionAcceleration(DEFAULT_ACCEL, MotorSettings.TIMEOUT);
-        leftMotor.configMotionCruiseVelocity(DEFAULT_VEL + (int) autoStraightModifier, MotorSettings.TIMEOUT);
-        leftMotor.configMotionAcceleration(DEFAULT_ACCEL, MotorSettings.TIMEOUT);
     }
-    
+
     /**
-     * Converts the geogebra data to targets for motors.
-     * <p>The returned array contains:
-     * <p>First index is left velocity
-     * <p>Second index is right velocity
+     * Ensures the robot moves with the angle from the geogebra data.
+     * <p>This changes the {@link #autoAngleModifier} so that the robot moves straight.
+     */
+    public double autoAngle(double targetAngle) {
+        PigeonIMU.FusionStatus fusionStatus = new PigeonIMU.FusionStatus();
+        double[] xyz_dps = new double[3];
+        pigeon.getRawGyro(xyz_dps);
+        pigeon.getFusedHeading(fusionStatus);
+
+        currentAngle = fusionStatus.heading;
+        currentAngularRate = xyz_dps[2];
+
+        autoAngleModifier =
+            (targetAngle - currentAngle) * AUTO_ANGLE_P - (currentAngularRate) * AUTO_ANGLE_D;
+        autoAngleModifier = limit(autoAngleModifier);
+
+        return autoAngleModifier;
+    }
+
+    /**
+     * Converts the geogebra data to targets for motors. 
+     * <p>The returned array contains: 
+     * <p>First index is left velocity 
+     * <p>Second index is right velocity 
      * <p>Third index is left acceleration
      * <p>Fourth index is right acceleration
      * 
@@ -106,48 +143,52 @@ public class AutoDrive {
      * @param targetPos the target position for the robot to go to.
      * @return the array containing motor targets.
      */
-    public int[] convertToMotorValues(double[] geogebra, int targetPos){
+    public int[] convertToMotorValues(double percentDiff, int targetPos) {
         int[] motorValues = new int[4];
-        
-        motorValues[0] = (int) ((1 + geogebra[determineInterval(targetPos, geogebra.length)]) * DEFAULT_VEL);
-        motorValues[1] = (int) ((1 - geogebra[determineInterval(targetPos, geogebra.length)]) * DEFAULT_VEL);
-        motorValues[2] = (int) ((1 + geogebra[determineInterval(targetPos, geogebra.length)]) * DEFAULT_ACCEL);
-        motorValues[3] = (int) ((1 - geogebra[determineInterval(targetPos, geogebra.length)]) * DEFAULT_ACCEL);
-        
+
+        motorValues[0] = (int) ((1 + percentDiff) * DEFAULT_VEL);
+        motorValues[1] = (int) ((1 - percentDiff) * DEFAULT_VEL);
+        motorValues[2] = (int) ((1 + percentDiff) * DEFAULT_ACCEL);
+        motorValues[3] = (int) ((1 - percentDiff) * DEFAULT_ACCEL);
+
         return motorValues;
     }
-    
+
     /**
      * Determines the interval that the robot is in right now.
+     * 
      * @param targetPos the target position for the robot to go to.
      * @return the interval that the robot is in right now.
      */
     public int determineInterval(int targetPos, int length) {
-        int averagePos = 
-            (leftMotor.getSelectedSensorPosition(MotorSettings.PID_IDX) + rightMotor.getSelectedSensorPosition(MotorSettings.PID_IDX)) / 2;
+        int averagePos = (leftMotor.getSelectedSensorPosition(MotorSettings.PID_IDX)
+            + rightMotor.getSelectedSensorPosition(MotorSettings.PID_IDX)) / 2;
         int interval = (averagePos * length) / targetPos;
         return interval;
     }
-    
+
     /**
      * Limits the modifier from being too big.
      * 
-     * <p>This compares the modifier with {@link #MAX_MODIFIER} and checks which one is smaller. It returns whatever is less.
+     * <p>This compares the modifier with {@link #MAX_MODIFIER} and checks which one is smaller. It
+     * returns whatever is less.
      * 
-     * <p>This is used instead of Math.min or Math.max because you don't have to worry about negatives.
+     * <p>This is used instead of Math.min or Math.max because you don't have to worry about
+     * negatives.
      * 
-     * @param value the value to compare with the {@link #MAX_MODIFIER} and select which ever is less
-     * @return the number to modify the velocity or acceleration 
+     * @param value the value to compare with the {@link #MAX_MODIFIER} and select which ever is
+     *            less
+     * @return the number to modify the velocity or acceleration
      */
     public double limit(double value) {
         if (value < -MAX_MODIFIER) {
             return -MAX_MODIFIER;
         }
-        
+
         if (value > +MAX_MODIFIER) {
             return +MAX_MODIFIER;
         }
-        
+
         return value;
     }
 }
