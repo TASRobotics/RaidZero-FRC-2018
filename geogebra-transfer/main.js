1c0e4942ef129db2f30ef50b7a7b8a8b1162aad9
qsctr <qsctr@users.noreply.github.com>
2018-01-31 14:09:24 +0800

    Rewrite transfer script

--- geogebra-transfer/main.js
+++ geogebra-transfer/main.js
@@ -2,104 +2,148 @@
 
 const directory = '/home/lvuser/';
 
+const pathInfoFormat = {
+    stage: nonNegativeNumber,
+    start: dependsOn('stage', stage =>
+        stage === 0 ? oneOf('left', 'center', 'right') : oneOf('left', 'right')),
+    end: oneOf('left', 'right'),
+    direction: oneOf('forward', 'backward')
+};
+
+const motorDataFormat = {
+    angle: number,
+    'percent difference': number
+};
+
 const clip = require('clipboardy');
-const conn = new require('ssh2').Client();
+const conn = require('ssh2').Client();
 
-console.log('Reading data from clipboard...');
-
-const [headerLine, ...rows] = clip.readSync().trim().split('\n');
-console.assert(rows.length >= 2, 'Data has less than 3 rows');
-
-const headerCells = headerLine.split('\t');
-console.assert(headerCells.length >= 3, 'Copied data has less than 3 columns');
-console.assert(headerCells.includes(''), 'Could not find blank column');
-console.assert(headerCells.indexOf('') > 3, 'Data for robot has less than 3 columns');
-const header = headerCells.slice(2, headerCells.indexOf('')).map(x => x.toLowerCase());
-console.assert(header.includes('angle'), 'Could not find angle');
-console.assert(header.includes('percent difference'), 'Could not find percent difference');
-
-const pathInfo = {};
-const rowInfo = [];
-
-for (let i = 0; i < rows.length; i++) {
-    const [prop, value, ...row] = rows[i].split('\t', header.length + 2);
-    if (prop !== '') {
-        const prop1 = prop.toLowerCase();
-        console.assert(pathInfo[prop1] === undefined, `Duplicate path property ${prop}`);
-        console.assert(value !== '', `Property ${prop} has no value`);
-        pathInfo[prop1] = value;
-    }
-    if (row[0] === '?') {
-        break;
-    }
-    const rowObj = {};
-    row.forEach((x, j) => {
-        console.assert(!Number.isNaN(parseFloat(x)),
-            `${x} (row ${i} of ${header[j]}) is not a number`);
-        console.assert(rowObj[header[j]] === undefined, `Duplicate column ${header[j]}`);
-        rowObj[header[j]] = x;
-    });
-    rowInfo.push(rowObj);
-}
-
-const pathData = [];
-
-console.assert(pathInfo.stage !== undefined, `Could not find stage`);
-const stage = parseFloat(pathInfo.stage);
-console.assert(!Number.isNaN(stage) && stage >= 0,
-    `Stage "${pathInfo.stage}" is not a positive number`);
-pathData.push(pathInfo.stage);
-
-console.assert(pathInfo.start !== undefined, `Could not find start`);
-const start1 = pathInfo.start.toLowerCase();
-if (stage === 0) {
-    console.assert(['left', 'center', 'right'].includes(start1),
-        `Start "${pathInfo.start}" should be left, center, or right for stage 0`);
-} else {
-    console.assert(['left', 'right'].includes(start1),
-        `Start "${pathInfo.start}" should be left or right for stage 1+`);
-}
-pathData.push(start1);
-
-console.assert(pathInfo.end !== undefined, `Could not find end`);
-const end1 = pathInfo.end.toLowerCase();
-console.assert(['left', 'right'].includes(end1),
-    `End "${pathInfo.end}" should be left or right`);
-pathData.push(end1);
-
-const rowData = rowInfo.map(x => [x.angle, x['percent difference']]);
-
-const data = pathData.concat(rowData).join('\n');
-const filename =
-    `${directory}${stage}-${start1}-${end1}.csv`;
-
+console.log('Reading data...');
+const [header, bodyLines] = readData(clip.readSync());
+console.log('Converting data...');
+const [filename, output] = convertData(header, bodyLines);
 console.log('Connecting to robot...');
+upload(filename, output);
 
-conn.on('ready', () => {
-    console.log('Initializing SFTP...');
-    conn.sftp((err, sftp) => {
-        if (err) throw err;
-        console.log(`Opening file ${filename}...`);
-        sftp.open(filename, 'w', (err, handle) => {
+function readData(str) {
+    const [headerLine, ...bodyLines] = str.trim().split('\n');
+    console.assert(bodyLines.length >= 2, 'Data has less than 3 rows');
+    const headerCells = headerLine.split('\t');
+    console.assert(headerCells.length >= 3, 'Data has less than 3 columns');
+    const emptyColumn = headerCells.indexOf('');
+    console.assert(emptyColumn >= 0, 'Could not find empty column');
+    console.assert(emptyColumn > 3, 'Data for robot has less than 3 columns');
+    const header = headerCells.slice(2, emptyColumn).map(x => x.toLowerCase());
+    checkSame(Object.keys(motorDataFormat), header, 'column');
+    return [header, bodyLines];
+}
+
+function convertData(header, lines) {
+    const pathInfoMap = {};
+    const motorDataOutput = [];
+    const columnCount = header.length + 2;
+    for (let i = 0; i < lines.length; i++) {
+        const [prop, value, ...motorData] =
+            lines[i].split('\t', columnCount).map(x => x.toLowerCase());
+        if (prop !== '') {
+            console.assert(pathInfoMap[prop] === undefined, `Duplicate ${prop}`);
+            console.assert(value !== '', `${prop} has no value`);
+            pathInfoMap[prop] = value;
+        }
+        if (motorData[0] === '?') {
+            break;
+        }
+        const motorDataMap = {};
+        motorData.forEach((x, j) => {
+            motorDataMap[header[j]] = x;
+        });
+        const motorDataMap1 =
+            verifyFormat(motorDataFormat, motorDataMap, p => `row ${i + 1} of ${p}`);
+        motorDataOutput.push(Object.keys(motorDataFormat).map(p => motorDataMap1[p]).join());
+    }
+    checkSame(Object.keys(pathInfoFormat), Object.keys(pathInfoMap), 'path info');
+    const pathInfoMap1 = verifyFormat(pathInfoFormat, pathInfoMap);
+    return [getFilename(pathInfoMap1), Object.keys(pathInfoFormat)
+        .map(prop => pathInfoMap1[prop]).concat(motorDataOutput).join('\n')]
+}
+
+function getFilename(pathInfo) {
+    return `${pathInfo.stage}-${pathInfo.start}-${pathInfo.end}.csv`;
+}
+
+function checkSame(format, actual, name) {
+    for (const x of format) {
+        console.assert(actual.includes(x), `Could not find ${name} ${x}`);
+    }
+    for (const x of actual) {
+        console.assert(format.includes(x), `Extra ${name} ${x}`);
+    }
+}
+
+function verifyFormat(format, obj, nameFunc = name => name) {
+    const result = {};
+    for (const prop in obj) {
+        result[prop] = format[prop](obj[prop], nameFunc(prop), obj);
+    }
+    return result;
+}
+
+function upload(filename, data) {
+    conn.on('ready', () => {
+        console.log('Initializing SFTP...');
+        conn.sftp((err, sftp) => {
             if (err) throw err;
-            const buffer = Buffer.from(data);
-            console.log('Writing data...');
-            sftp.write(handle, buffer, 0, buffer.length, 0, err => {
+            // sftp.mkdir('/home/lvuser/paths', err => {
+            //     if (err) throw err;
+            //     console.log('success');
+            //     conn.end();
+            // });
+            const filepath = directory + filename;
+            console.log(`Opening file ${filepath}...`);
+            sftp.open(filepath, 'w', (err, handle) => {
                 if (err) throw err;
-                console.log('Successfully wrote data:');
-                console.log(data);
-                console.log('Closing file...');
-                sftp.close(handle, err => {
+                const buffer = Buffer.from(data);
+                console.log('Writing data...');
+                sftp.write(handle, buffer, 0, buffer.length, 0, err => {
                     if (err) throw err;
-                    conn.end();
-                    console.log('Successfully transferred data to robot');
+                    console.log('Successfully wrote data:');
+                    console.log(data);
+                    console.log('Closing file...');
+                    sftp.close(handle, err => {
+                        if (err) throw err;
+                        conn.end();
+                        console.log('Successfully transferred data to robot');
+                    });
                 });
             });
         });
+    }).connect({
+        host: 'roboRIO-4253-FRC.local',
+        port: 22,
+        username: 'lvuser',
+        password: ''
     });
-}).connect({
-    host: 'roboRIO-4253-FRC.local',
-    port: 22,
-    username: 'lvuser',
-    password: ''
-});
+}
+
+function number(x, name) {
+    const n = parseFloat(x);
+    console.assert(!Number.isNaN(n), `${name} is not a number`);
+    return n;
+}
+
+function nonNegativeNumber(x, name) {
+    const n = number(x, name);
+    console.assert(n >= 0, `${name} should be a positive number`);
+    return n;
+}
+
+function oneOf(...xs) {
+    return (x, name) => {
+        console.assert(xs.includes(x), `${name} = ${x} but should be ${xs.join(' or ')}`);
+        return x;
+    };
+}
+
+function dependsOn(prop, f) {
+    return (x, name, obj) => f(obj[prop])(x, name);
+}
